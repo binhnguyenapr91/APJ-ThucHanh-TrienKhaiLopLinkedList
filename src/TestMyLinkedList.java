c5ff97fe854a737e85cf776186741f0baa23a7be
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-04-02 13:13:16 +0700

    finished

---
+++ src/TestMyLinkedList.java
@@ -0,0 +1,17 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.addFirst(6);
+
+        ll.add(4, 45);
+        ll.printList();
+        
+    }
+
+}
